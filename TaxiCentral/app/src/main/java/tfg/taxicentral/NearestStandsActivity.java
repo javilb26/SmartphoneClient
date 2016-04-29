6991b0e65d5f93ebbdf6d98b4569513e7028ed6d
Javier Losa Balsa <javier.losa.balsa@udc.es>
2016-04-29 19:23:52 +0200

    Arreglado NearestStands

--- TaxiCentral/app/src/main/java/tfg/taxicentral/NearestStandsActivity.java
+++ TaxiCentral/app/src/main/java/tfg/taxicentral/NearestStandsActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -21,7 +22,10 @@ import java.util.logging.Logger;
 public class NearestStandsActivity extends ListActivity {
 
     private NearestStandsTask mNearestStandsTask = null;
+    private NumTaxisStandTask mNumTaxisStandTask = null;
     String[] stand;
+    String numTaxis;
+    int nearestStandsTaskFlag = 0, numTaxisStandTaskFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,13 @@ public class NearestStandsActivity extends ListActivity {
         mNearestStandsTask = new NearestStandsTask(getSharedPreferences("credentials", getApplicationContext().MODE_PRIVATE).getLong("taxiId", 0));
         mNearestStandsTask.execute((Void) null);
         //TODO resolver sin recurrir a sleeps
-        try {
+        /*try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }*/
+        while (nearestStandsTaskFlag == 0) {
+
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, stand);
@@ -44,8 +51,21 @@ public class NearestStandsActivity extends ListActivity {
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
+        numTaxisStandTaskFlag = 0;
+        Log.e("NearestStandsActivity", "item pulsado");
         String item = (String) getListAdapter().getItem(position);
-        super.finish();
+        Log.e("NearestStandsActivity", item.substring(0,1));
+        mNumTaxisStandTask = new NumTaxisStandTask(new Long(item.substring(0,1)));
+        mNumTaxisStandTask.execute((Void) null);
+        //TODO Arreglar actualizacion
+        while (numTaxisStandTaskFlag == 0) {
+
+        }
+        if (numTaxis.compareTo("")==0) {
+            numTaxis = "0";
+        }
+        Toast.makeText(getApplicationContext(), numTaxis, Toast.LENGTH_SHORT).show();
+        //super.finish();
     }
 
     public class NearestStandsTask extends AsyncTask<Void, Void, Boolean> {
@@ -67,8 +87,10 @@ public class NearestStandsActivity extends ListActivity {
                 stand = new String[respJSON.length()];
                 for (int i = 0; i < respJSON.length(); i++) {
                     JSONObject obj = respJSON.getJSONObject(i);
-                    stand[i] = "'" + obj.getString("name") + "'";
-                    Log.e("NearestStands","Stand: " + stand[i].toString());
+                    //TODO mirar si lo ideal seria que teniendo un array de ids (manteniendo la misma i que para stand apareciera ya en view taxi stands el numero de taxis en parada
+                    stand[i] = obj.getLong("standId") + " - '" + obj.getString("name") + "'";
+                    nearestStandsTaskFlag = 1;
+                    //Log.e("NearestStands","Stand: " + stand[i].toString());
                 }
 
             } catch (Exception ex) {
@@ -80,4 +102,28 @@ public class NearestStandsActivity extends ListActivity {
 
     }
 
+    public class NumTaxisStandTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final Long mStandId;
+
+        NumTaxisStandTask(Long standId) {
+            mStandId = standId;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            HttpGet get = new HttpGet(getString(R.string.ip) + "stand/" + mStandId + "/numtaxis");
+            get.setHeader("content-type", "application/json");
+            try {
+                HttpResponse resp = new DefaultHttpClient().execute(get);
+                numTaxis = EntityUtils.toString(resp.getEntity());
+                numTaxisStandTaskFlag = 1;
+            } catch (Exception ex) {
+                Log.e("ServicioRest", "Error!", ex);
+                return false;
+            }
+            return true;
+        }
+
+    }
 }

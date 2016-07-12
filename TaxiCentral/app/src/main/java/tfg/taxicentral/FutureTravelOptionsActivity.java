7cd059716f031e8145d22d6f604caa901228b40d
Javier Losa Balsa <javier.losa.balsa@udc.es>
2016-07-12 22:25:51 +0200

    Añadido FutureTravelOptionsActivity y modificado FutureTravelsActivity

---
+++ TaxiCentral/app/src/main/java/tfg/taxicentral/FutureTravelOptionsActivity.java
@@ -0,0 +1,159 @@
+package tfg.taxicentral;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class FutureTravelOptionsActivity extends AppCompatActivity {
+
+    private TakeClientToFromFutureTravelTask mTakeClientToFromFutureTravelTask = null;
+    private Long travelId = (long) 0;
+    private CancelFutureTravelTask mCancelFutureTravelTask = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future_travel_options);
+
+        TextView item = (TextView) findViewById(R.id.itemTextView);
+        item.setText(getIntent().getStringExtra("item"));
+
+        Button mGoToButton = (Button) findViewById(R.id.goToButton);
+        mGoToButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = getIntent().getStringExtra("originAddressStr") + ", " + getIntent().getStringExtra("originCityStr") + ", " + getIntent().getStringExtra("originRegionStr") + ", " + getIntent().getStringExtra("originCountryStr");
+                Log.e("GoTo", url);
+                //TODO Rectificar los tiempos, se ejecuta el travelId antes que la creacion del travel -> asegurarse
+                mTakeClientToFromFutureTravelTask = new TakeClientToFromFutureTravelTask(getSharedPreferences("credentials", getApplicationContext().MODE_PRIVATE).getLong("taxiId", 0),
+                        getIntent().getLongExtra("originCountryId",0), getIntent().getLongExtra("originRegionId",0), getIntent().getLongExtra("originCityId", 0), getIntent().getLongExtra("originAddressId",0),
+                        getIntent().getLongExtra("destinationCountryId",0), getIntent().getLongExtra("destinationRegionId",0), getIntent().getLongExtra("destinationCityId",0), getIntent().getLongExtra("destinationAddressId",0));
+                mTakeClientToFromFutureTravelTask.execute((Void) null);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    List<Address> geocodeMatches = new Geocoder(getApplicationContext()).getFromLocationName(url, 1);
+                    if (geocodeMatches != null) {
+                        Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+                        intent.putExtra("url", url);
+                        intent.putExtra("lat", geocodeMatches.get(0).getLatitude());
+                        intent.putExtra("lng", geocodeMatches.get(0).getLongitude());
+                        intent.putExtra("travelId", travelId);
+                        intent.putExtra("routing", true);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Destino no encontrado", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "No se pudo convertir la direccion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button mDeleteButton = (Button) findViewById(R.id.deleteButton);
+        mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCancelFutureTravelTask != null) {
+                    return;
+                }
+                mCancelFutureTravelTask = new CancelFutureTravelTask(getIntent().getLongExtra("futureTravelId",0));
+                mCancelFutureTravelTask.execute((Void) null);
+                Toast.makeText(getApplicationContext(), "FutureTravel cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    public class TakeClientToFromFutureTravelTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final Long mTaxiId, mOriginCountryId, mOriginRegionId, mOriginCityId, mOriginAddressId, mDestinationCountryId, mDestinationRegionId, mDestinationCityId, mDestinationAddressId;
+
+        TakeClientToFromFutureTravelTask(Long taxiId, Long originCountryId, Long originRegionId, Long originCityId, Long originAddressId, Long destinationCountryId, Long destinationRegionId, Long destinationCityId, Long destinationAddressId) {
+            mTaxiId = taxiId;
+            mOriginCountryId = originCountryId;
+            mOriginRegionId = originRegionId;
+            mOriginCityId = originCityId;
+            mOriginAddressId = originAddressId;
+            mDestinationCountryId = destinationCountryId;
+            mDestinationRegionId = destinationRegionId;
+            mDestinationCityId = destinationCityId;
+            mDestinationAddressId = destinationAddressId;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            HttpPut put = new HttpPut(getString(R.string.ip) + "taxis/" + mTaxiId + "/origincountries/" + mOriginCountryId + "/originregions/" + mOriginRegionId
+                    + "/origincities/" + mOriginCityId + "/originaddresses/" + mOriginAddressId + "/destinationcountries/" + mDestinationCountryId + "/destinationregions/" + mDestinationRegionId
+                    + "/destinationcities/" + mDestinationCityId + "/destinationaddresses/" + mDestinationAddressId);
+            put.setHeader("content-type", "application/json");
+            try {
+                HttpResponse resp = new DefaultHttpClient().execute(put);
+                String respStr = EntityUtils.toString(resp.getEntity());
+                travelId = Long.valueOf(respStr);
+            } catch (Exception ex) {
+                Log.e("ServicioRest", "Error!", ex);
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    public class CancelFutureTravelTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final Long mFutureTravelId;
+
+        CancelFutureTravelTask(Long futureTravelId) {
+            mFutureTravelId = futureTravelId;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            HttpDelete delete = new HttpDelete(getString(R.string.ip) + "futuretravels/" + mFutureTravelId);
+            delete.setHeader("content-type", "application/json");
+            try {
+                HttpResponse resp = new DefaultHttpClient().execute(delete);
+                String respStr = EntityUtils.toString(resp.getEntity());
+                if (!respStr.equals("true"))
+                    return false;
+            } catch (Exception ex) {
+                Log.e("ServicioRest", "Error!", ex);
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
--- TaxiCentral/app/src/main/java/tfg/taxicentral/FutureTravelsActivity.java
+++ TaxiCentral/app/src/main/java/tfg/taxicentral/FutureTravelsActivity.java
@@ -1,10 +1,14 @@
 package tfg.taxicentral;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -20,6 +24,8 @@ public class FutureTravelsActivity extends ListActivity {
     String[] futureTravels;
     String numTaxis;
     int futureTravelsTaskFlag = 0, numTaxisStandTaskFlag = 0;
+    long[] futureTravelId, originCountryId, originRegionId, originCityId, originAddressId, destinationCountryId, destinationRegionId, destinationCityId, destinationAddressId;
+    String[] originCountryStr, originRegionStr, originCityStr, originAddressStr, destinationCountryStr, destinationRegionStr, destinationCityStr, destinationAddressStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,27 +48,35 @@ public class FutureTravelsActivity extends ListActivity {
                 android.R.layout.simple_list_item_1, futureTravels);
         setListAdapter(adapter);
     }
-/*
-    //TODO Al clickar en un travel -> mostrar en Navigation la ruta hecha
+
+    //Ir a
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        numTaxisStandTaskFlag = 0;
-        Log.e("NearestStandsActivity", "item pulsado");
         String item = (String) getListAdapter().getItem(position);
-        Log.e("NearestStandsActivity", item.substring(0,1));
-        mNumTaxisStandTask = new NumTaxisStandTask(new Long(item.substring(0,1)));
-        mNumTaxisStandTask.execute((Void) null);
-        //TODO Arreglar actualizacion
-        while (numTaxisStandTaskFlag == 0) {
-
-        }
-        if (numTaxis.compareTo("")==0) {
-            numTaxis = "0";
-        }
-        Toast.makeText(getApplicationContext(), numTaxis, Toast.LENGTH_SHORT).show();
-        //super.finish();
+        //Esta clase tendra arriva del toodo un textview con la info y dos botones, uno para ir y otro para borrar
+        Intent intent = new Intent(getApplicationContext(), FutureTravelOptionsActivity.class);
+            intent.putExtra("item", item);
+            intent.putExtra("futureTravelId", futureTravelId[position]);
+            intent.putExtra("originCountryStr", originCountryStr[position]);
+            intent.putExtra("originRegionStr", originRegionStr[position]);
+            intent.putExtra("originCityStr", originCityStr[position]);
+            intent.putExtra("originAddressStr", originAddressStr[position]);
+            intent.putExtra("destinationCountryStr", destinationCountryStr[position]);
+            intent.putExtra("destinationRegionStr", destinationRegionStr[position]);
+            intent.putExtra("destinationCityStr", destinationCityStr[position]);
+            intent.putExtra("destinationAddressStr", destinationAddressStr[position]);
+        intent.putExtra("originCountryId", originCountryId[position]);
+        intent.putExtra("originRegionId", originRegionId[position]);
+        intent.putExtra("originCityId", originCityId[position]);
+        intent.putExtra("originAddressId", originAddressId[position]);
+        intent.putExtra("destinationCountryId", destinationCountryId[position]);
+        intent.putExtra("destinationRegionId", destinationRegionId[position]);
+        intent.putExtra("destinationCityId", destinationCityId[position]);
+        intent.putExtra("destinationAddressId", destinationAddressId[position]);
+        startActivity(intent);
+        finish();
     }
-*/
+
     public class FutureTravelsTask extends AsyncTask<Void, Void, Boolean> {
 
         private final Long mTaxiId;
@@ -80,6 +94,23 @@ public class FutureTravelsActivity extends ListActivity {
                 String respStr = EntityUtils.toString(resp.getEntity());
                 JSONArray respJSON = new JSONArray(respStr);
                 futureTravels = new String[respJSON.length()];
+                futureTravelId = new long[99999];
+                originCountryStr = new String[99999];
+                originRegionStr = new String[99999];
+                originCityStr = new String[99999];
+                originAddressStr = new String[99999];
+                destinationCountryStr = new String[99999];
+                destinationRegionStr = new String[99999];
+                destinationCityStr = new String[99999];
+                destinationAddressStr = new String[99999];
+                originCountryId = new long[99999];
+                originRegionId = new long[99999];
+                originCityId = new long[99999];
+                originAddressId = new long[99999];
+                destinationCountryId = new long[99999];
+                destinationRegionId = new long[99999];
+                destinationCityId = new long[99999];
+                destinationAddressId = new long[99999];
                 for (int i = 0; i < respJSON.length(); i++) {
                     JSONObject obj = respJSON.getJSONObject(i);
                     //Log.e("HistoryActivity",obj.toString());
@@ -95,6 +126,24 @@ public class FutureTravelsActivity extends ListActivity {
                     futureTravels[i] = obj.getLong("futureTravelId") + " - '" + obj.getString("date") + "'" + " - '" + originCountry.getString("name") + "'" + " - '" + originRegion.getString("name") + "'" + " - '" + originCity.getString("name") + "'" + " - '" + originAddress.getString("name") + "'" + " - '" + destinationCountry.getString("name") + "'" + " - '" + destinationRegion.getString("name") + "'" + " - '" + destinationCity.getString("name") + "'" + " - '" + destinationAddress.getString("name") + "'";
                     futureTravelsTaskFlag = 1;
                     //Log.e("NearestStands","Stand: " + stand[i].toString());
+                    futureTravelId[i] = obj.getLong("futureTravelId");
+                    originCountryStr[i] = originCountry.getString("name");
+                    originRegionStr[i] = originRegion.getString("name");
+                    originCityStr[i] = originCity.getString("name");
+                    originAddressStr[i] = originAddress.getString("name");
+                    destinationCountryStr[i] = destinationCountry.getString("name");
+                    destinationRegionStr[i] = destinationRegion.getString("name");
+                    destinationCityStr[i] = destinationCity.getString("name");
+                    destinationAddressStr[i] = destinationAddress.getString("name");
+
+                    originCountryId[i] = originCountry.getLong("countryId");
+                    originRegionId[i] = originRegion.getLong("regionId");
+                    originCityId[i] = originCity.getLong("cityId");
+                    originAddressId[i] = originAddress.getLong("addressId");
+                    destinationCountryId[i] = destinationCountry.getLong("countryId");
+                    destinationRegionId[i] = destinationRegion.getLong("regionId");
+                    destinationCityId[i] = destinationCity.getLong("cityId");
+                    destinationAddressId[i] = destinationAddress.getLong("addressId");
                 }
 
             } catch (Exception ex) {
